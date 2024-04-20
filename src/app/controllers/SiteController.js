eb788a133dc9c79ba1d29ba89a37e876325399a6
Gnoud <gnoudeath@gmail.com>
2024-04-20 11:51:58 +0700

    Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,22 +1,21 @@
-const Course = require('../models/Course')
+const Course = require("../models/Course");
+const { mutipleMongooseToObject } = require("../../util/mongoose");
 
 class SiteController {
-    // [GET] /
-    async index(req, res) {
-        try {
-            const courses = await Course.find({});
-            res.json(courses);
-        } catch (err) {
-            res.status(400).json({ error: 'Error!!!' });
-        }
-    }
+  // [GET] /
+  index(req, res, next) {
+    Course.find({})
+      .then(courses => {
+        res.render("home", { courses: mutipleMongooseToObject(courses)
+        });
+  })
+      .catch(next);
+  }
 
-    // [GET] /search
-    search(req, res) {
-        res.render('search');
-    }
-};
+  // [GET] /search
+  search(req, res) {
+    res.render("search");
+  }
+}
 
-
-
-module.exports = new SiteController;
+module.exports = new SiteController();
